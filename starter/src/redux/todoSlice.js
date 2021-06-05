b7c9cd97603ed7017c82038acc8390efec0acb92
Daniel Torres <denial.torres@gmail.com>
2021-06-05 12:54:37 -0500

    add todoSlice to get the reducer and the actions

---
+++ starter/src/redux/todoSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todoSlice = createSlice({
+  name: "todos",
+  initialState: [
+    { id: 1, title: "todo1", completed: false },
+    { id: 2, title: "todo2", completed: false },
+    { id: 3, title: "todo3", completed: true },
+    { id: 4, title: "todo4", completed: false },
+    { id: 5, title: "todo5", completed: false },
+  ],
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: new Date(),
+        title: action.payload.title,
+        completed: false,
+      };
+      state.push(todo);
+    },
+  },
+});
+
+// So the todoSlice has created a bunch of actions
+// for us based on our reducer names, and we just use destructuring to get the addTodo action and export it
+export const { addTodo } = todoSlice.actions;
+
+// And we export the reducer so we can add it to our store (line 28).
+export default todoSlice.reducer;
